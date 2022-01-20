38925dff9a485e038a47c9ec313252f71f956b55
上田大樹 <d.ueda@blue-corporation.jp>
2022-01-20 20:15:40 +0900

    firstCommit

---
+++ LocationRecode/LocationRecodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationRecodeApp.swift
+//  LocationRecode
+//
+//  Created by 上田大樹 on 2022/01/17.
+//
+
+import SwiftUI
+
+@main
+struct LocationRecodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
